df336d37bbdf4b665034d63291603dc2d51b6b54
cathandstamp <72399484+cathandstamp@users.noreply.github.com>
2021-03-24 05:08:49 +0900

    studyyyy

---
+++ hi.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from glob import glob
+
+A = glob("*.csv")
+print(A)
+
+
+def search_CSV():
+    # 検索対象
+    #df=pd.read_csv("TEST.csv")
+    #source=list(df["name"])
+    print(search_CSV)
+
+
+
+if __name__ == "__main__":
+    search_CSV()
